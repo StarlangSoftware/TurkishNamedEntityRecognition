d3d94992bf8cf5213cdbb5a87d6836765453f847
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-06-04 07:17:25 +0300

    Updated the documentation.

--- src/main/java/NamedEntityRecognition/Slot.java
+++ src/main/java/NamedEntityRecognition/Slot.java
@@ -4,11 +4,24 @@ public class Slot {
     private SlotType type;
     private final String tag;
 
+    /**
+     * Constructor for the Slot object. Slot object stores the information about more specific entities. The slot
+     * type represents the beginning, inside or outside the slot, whereas tag represents the entity tag of the
+     * slot.
+     * @param type Type of the slot. B, I or O for beginning, inside, outside the slot respectively.
+     * @param tag Tag of the slot.
+     */
     public Slot(SlotType type, String tag){
         this.type = type;
         this.tag = tag;
     }
 
+    /**
+     * Second constructor of the slot for a given slot string. A Slot string consists of slot type and slot tag
+     * separated with '-'. For example B-Person represents the beginning of a person. For outside tagging simple 'O' is
+     * used.
+     * @param slot Input slot string.
+     */
     public Slot(String slot){
         if (slot.equalsIgnoreCase("O")){
             type = SlotType.O;
@@ -28,14 +41,26 @@ public class Slot {
         }
     }
 
+    /**
+     * Accessor for the type of the slot.
+     * @return Type of the slot.
+     */
     public SlotType getType(){
         return type;
     }
 
+    /**
+     * Accessor for the tag of the slot.
+     * @return Tag of the slot.
+     */
     public String getTag(){
         return tag;
     }
 
+    /**
+     * ToString method of the slot.
+     * @return Type and tag separated with '-'. If the type is outside, it returns 'O'.
+     */
     public String toString(){
         switch (type){
             case O:
